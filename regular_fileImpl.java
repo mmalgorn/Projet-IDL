f8c44c47993e8cec06c603b3ca6175549b7b5824
Mathieu GOURVES <mathieu.gourves1@etudiant.univ-brest.fr>
2017-01-24 17:44:09 +0100

    Le super commit !

--- regular_fileImpl.java
+++ regular_fileImpl.java
@@ -2,19 +2,39 @@ package tpcorba.projet;
 
 import org.omg.CORBA.*;
 import java.lang.*;
+import java.io.*;
 
 public class regular_fileImpl extends regular_filePOA {
 
-  public long read(long size, inout StringHolder data) {
+  int offset;
+  File file;
 
+  public regular_fileImpl (String pathname) {
+    file = new File(pathname);
   }
 
-  public long write(long size, String data) {
+  public int read(int size, StringHolder data) {
+    if (new_offset > file.length()) throw new end_of_file();
 
+    RandomAccessFile raf = new RandomAccessFile(file, "r");
+    byte[] buf;
+    try {
+      int nbr = frd.read(buf, offset, size);
+    } catch(IOException e) {
+      throw new invalid_operation();
+    }
+    data.value = new String(buf);
+    return nbr;
   }
 
-  public void seek(long new_offset) {
+  public int write(int size, String data) {
+    if (new_offset > file.length()) throw new end_of_file();
 
+    FileWriter frd = new FileWriter(file);
+  }
+
+  public void seek(int new_offset) {
+    if (new_offset > file.length()) throw new end_of_file();
   }
 
   public void close() {
