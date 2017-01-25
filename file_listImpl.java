33cce89008df32b5d80df15d2d0a2f6644d5a1d5
Mathieu GOURVES <mathieu.gourves1@etudiant.univ-brest.fr>
2017-01-25 14:37:39 +0100

    file_listImpl.java

---
+++ file_listImpl.java
@@ -0,0 +1,12 @@
+package tpcorba.projet;
+
+import org.omg.CORBA.*;
+import java.lang.*;
+
+public class file_listImpl extends file_listPOA {
+
+  public boolean next_one(directory_entryHolder e) {
+
+  }
+
+}
