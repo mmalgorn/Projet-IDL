6e057599fe960b86356bc8b3c81848781bf3a282
Mathieu GOURVES <mathieu.gourves1@etudiant.univ-brest.fr>
2017-01-25 15:35:45 +0100

    Thecommit

--- directoryImpl.java
+++ directoryImpl.java
@@ -13,26 +13,45 @@ public class directoryImpl extends directoryPOA {
   }
 
   public void open_regular_file(regular_fileHolder r, String name, mode m) {
+      File f = new File(name);
+      if (!f.exists()) throw new no_such_file();
+      if (!f.isFile()) throw new invalid_type_file();
 
+      r.value = new regular_fileImpl(f);
   }
 
   public void open_directory(directoryHolder f, String name) {
+      File f = new File(name);
+      if (!f.exists()) throw new no_such_file();
+      if (!f.isDirectory()) throw new invalid_type_file();
 
+      r.value = new directoryImpl(f);
   }
 
   public void create_regular_file(regular_fileHolder r, String name) {
+      File f = new File(name);
+      if (f.exists()) throw new already_exists();
 
+      f.createNewFile();
+      r.value = new regular_fileImpl(f);
   }
 
   public void create_directory(directoryHolder f, String name) {
+      File f = new File(name);
+      if (f.exists()) throw new already_exists();
 
+      f.mkdir();
+      r.value = new directoryImpl(f);
   }
 
   public void delete_file(String name) {
+      File f = new File(name);
+      if (f.exists()) throw new no_such_file();
 
+      f.destroy();
   }
 
   public long list_files(file_listHolder l) {
-
+      
   }
 }
